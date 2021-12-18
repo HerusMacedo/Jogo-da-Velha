25435070328e241a8e87682304608ed1f27d519c
Herus Macedo <herus2019@hotmail.com>
2021-12-18 11:29:55 -0300

    ok

--- game.js
+++ game.js
@@ -4,3 +4,21 @@ let tabuleiro = ['','','','','','','','',''];
 let vezJogador = 0;
 
 let simbolos = ['o', 'x'];
+
+
+function handleMove(position){
+
+
+tabuleiro(position) = simbolos[vezJogador]
+
+if (playerTime == 0 ) {
+
+    playerTime = 1;
+
+}else{
+
+    playerTime = 0;
+
+}
+
+}
--- interface.js
+++ interface.js
@@ -1,8 +1,52 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
-let peca = document.querySelectorAll(".peca");
+let quadrados = document.querySelectorAll(".quadrado");
 
-peca.forEach
+quadrados.forEach((quadrado)=>{
+
+quadrado.addEventListener('click', handleClick);
+
 
 
 })
+
+
+})
+
+function handleClick(event) {
+
+
+
+
+let quadrado = event.target;
+
+let postion = quadrado.id;
+
+handleMovr(position);
+updateSquares();
+
+}
+
+
+function updateSquares(){
+
+    let quadrados = document.querySelectorAll(".quadrado");
+
+    quadrados.forEach((quadrado)=>{
+    
+let postion = quadrado.id;
+
+ let simbolo = tabuleiro[postion]
+    
+ if(symbol !=''){
+
+quadrado.innerHTML = `<div class='${simbolo}'></div>`
+
+ }
+
+
+
+    })
+
+
+}
